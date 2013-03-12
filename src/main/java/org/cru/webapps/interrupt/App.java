3eb90c76f999eeee95a83f031690ae96d660a9fb
Robby Ronk <robby.ronk@gmail.org>
2013-03-12 16:23:34 -0400

    Add shouldInterrupt api definition

--- src/main/java/org/cru/webapps/interrupt/App.java
+++ src/main/java/org/cru/webapps/interrupt/App.java
@@ -1,7 +1,17 @@
 package org.cru.webapps.interrupt;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 @ApplicationPath("api/")
 public class App extends javax.ws.rs.core.Application {
+
+    @GET
+    @Path("{ssoGuid}")
+    public boolean shouldInterrupt(@PathParam("ssoGuid") String ssoGuid) {
+        return true;
+    }
+
 }
