c0c0546961de54cb33ecc689e04f827580852af8
Robby Ronk <robby.ronk@gmail.org>
2013-03-18 14:18:48 -0400

    Change SsoGuid to be a value object produced by SsoGuidFromSession

--- src/main/java/org/cru/webapps/interrupt/sua/SignatureDao.java
+++ src/main/java/org/cru/webapps/interrupt/sua/SignatureDao.java
@@ -1,8 +1,11 @@
 package org.cru.webapps.interrupt.sua;
 
+import org.cru.webapps.interrupt.sua.auth.SsoGuid;
+
 import javax.ejb.Stateful;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import java.util.Date;
 import java.util.List;
 
 @Stateful
@@ -11,18 +14,21 @@ public class SignatureDao {
     @Inject
     EntityManager em;
 
-    public Signature saveSignature(String ssoGuid) {
+    public Signature saveSignature(SsoGuid ssoGuid) {
         final Signature signature = new Signature();
 
-        signature.setSsoGuid(ssoGuid);
+        signature.setSsoGuid(ssoGuid.get());
+        signature.setDateSigned(new Date());
 
         em.persist(signature);
 
         return signature;
     }
 
-    public boolean shouldInterrupt(String ssoGuid) {
-        final List<Signature> resultList = em.createQuery("select s from Signature s where s.ssoGuid = :ssoGuid", Signature.class)
+    public boolean shouldSign(String ssoGuid) {
+        //TODO add expiration time here
+        final List<Signature> resultList = em.createQuery("select s from Signature s " +
+                "where s.ssoGuid = :ssoGuid", Signature.class)
                 .setParameter("ssoGuid", ssoGuid)
                 .getResultList();
         return resultList.isEmpty();
