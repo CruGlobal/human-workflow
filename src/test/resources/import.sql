a5980c159bb1bc9f213eeeb74b14f0ef1eb677b5
robbyronk <robby.ronk@gmail.com>
2013-04-05 10:57:02 +0000

    Increase IDs of test objects to not interfere with sequence

--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -1,4 +1,2 @@
-BEGIN
-insert into Signature (id, ssoGuid, dateSigned) values (1, 'abc-123', '2013-01-01')
-insert into Signature (id, ssoGuid, dateSigned) values (2, 'old-987', '2010-01-01')
-commit
+INSERT INTO Signature (id, ssoGuid, dateSigned) VALUES (1000, 'abc-123', '2013-01-01');
+insert into Signature (id, ssoGuid, dateSigned) values (1001, 'old-987', '2010-01-01');
