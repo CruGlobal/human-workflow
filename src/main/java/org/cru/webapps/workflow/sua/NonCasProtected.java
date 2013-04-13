bc734109ef16a8cb9028f0ea52d6f37e11260deb
robbyronk <robby.ronk@gmail.com>
2013-04-13 09:29:22 +0000

    Log info about headers and return status

--- src/main/java/org/cru/webapps/workflow/sua/NonCasProtected.java
+++ src/main/java/org/cru/webapps/workflow/sua/NonCasProtected.java
@@ -5,8 +5,10 @@ import org.cru.webapps.workflow.sua.util.EncryptedProperties;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
-
+import java.util.Set;
 import java.util.logging.Logger;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -29,9 +31,16 @@ public class NonCasProtected {
     @GET
     @Path("/{ssoGuid}")
     public boolean requireRedirect(@HeaderParam("serverId") String serverId,
-                                   @HeaderParam("serverSecret") String secret,
-                                   @PathParam("ssoGuid") String ssoGuid) {
-        // TODO remove log statements
+                                    @HeaderParam("serverSecret") String secret,
+                                    @PathParam("ssoGuid") String ssoGuid,
+                                    @Context HttpHeaders httpHeaders) {
+        Set<String> headers = httpHeaders.getRequestHeaders().keySet();
+
+        logger.info("Headers:");
+        for (String header : headers) {
+            logger.info("  " + header);
+        }
+
         try {
             checkNotNull(serverId);
             checkNotNull(secret);
@@ -54,6 +63,7 @@ public class NonCasProtected {
     }
 
     private void returnStatus(Status status) throws WebApplicationException {
+        logger.severe("STATUS RETURNED: " + status.toString());
         ResponseBuilder builder = status(status);
         builder.type("application/json");
         builder.entity(null);
